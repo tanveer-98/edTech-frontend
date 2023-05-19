ac3f11f7cb017e2f1bd4c51d58b5d51fed794f25
tanveer-98 <tnvrahmed98@gmail.com>
2023-05-20 01:48:12 +0530

    feat : added gallery modal

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -2,35 +2,94 @@ import React, { useState } from "react";
 import Tilt from "react-parallax-tilt";
 const photos = ["photo1.jpeg", "photo2.jpeg", "photo3.jpeg", "photo4.jpeg"];
 import { toggleScrollbar } from "../functions/toggleScrollbar";
+import { AiOutlineClose } from "react-icons/ai";
+
 const Gallery = () => {
   const [modal, setModal] = useState(false);
-  const [current, setCurrent] = useState<string|null>(null);
+  const [current, setCurrent] = useState<string | undefined>(undefined);
 
-  const toggleModal = (photo: string )=>{
+  const toggleModal = (photo: string = "https://picsum.photos/200/300") => {
     setModal(!modal);
-    setCurrent(photo)
-    // toggleScrollbar();
-  }
+    setCurrent(photo);
+    toggleScrollbar();
+  };
   return (
     <>
       <div
-        className={`fixed hidden top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-5rem)] max-h-[80%] justify-center items-center bg-yellow-400 ${
-          modal ? "hidden" : "hidden"
+        // className="w-screen h-screen flex justify-center items-center"
+        onClick ={()=>toggleModal("null")}
+        className={`fixed flex justify-center items-center backdrop-blur-lg top-0 left-0 right-0 z-50  w-screen p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full ${
+          modal ? "flex" : "hidden"
         }`}
       >
-        <h3>Modal Active</h3>
+        <div className="backdrop-blur-md">
+          <div className="relative w-full max-w-2xl max-h-full">
+            <div className="relative bg-[#414040f8] rounded-lg shadow dark:bg-gray-700">
+              <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                  
+                </h3>
+                <button
+                  type="button"
+                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  data-modal-hide="defaultModal"
+                  onClick={()=>toggleModal("happy")}
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span className="sr-only">Close modal</span>
+                </button>
+              </div>
+
+              <div className="p-6 space-y-6">
+              <img className="h-[100px] w-[100px] md:h-[500px] md:w-[500px] object-cover" src={current} />
+              </div>
+{/* 
+              <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <button
+                  data-modal-hide="defaultModal"
+                  type="button"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  I accept
+                </button>
+                <button
+                  data-modal-hide="defaultModal"
+                  type="button"
+                  className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                >
+                  Decline
+                </button>
+              </div> */}
+            </div>
+          </div>
+        </div>
       </div>
       <div className="mt-16">
         <div className="flex justify-center items-center">
           <h3>Gallery</h3>
         </div>
-        <div className="flex min-h-screen w-full flex-wrap content-center justify-center p-5 bg-transparent">
+        <div className="flex min-h-screen w-full flex-wrap content-center justify-center p-2 bg-transparent">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
             {photos.map((photo) => {
               return (
                 <>
                   <Tilt>
-                    <div onClick ={()=>toggleModal(photo)} className="w-60 md:w-80 hover:scale-110 hover:cursor-pointer  transition-all duration-200 ease-linear bg-transparent p-3">
+                    <div
+                      onClick={() => toggleModal(photo)}
+                      className="w-60 md:w-80 hover:scale-110 hover:cursor-pointer  transition-all duration-200 ease-linear bg-transparent p-3"
+                    >
                       <img className="h-52 w-full object-cover" src={photo} />
                       {/* <ul className="mt-3 flex flex-wrap">
                         <li className="mr-auto">
